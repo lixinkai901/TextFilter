b33919f7d6b73aceca329f8a4a717c171182730f
David <lixinkai901@gmail.com>
2017-04-08 17:49:48 -0400

    Version 1.0
    Create Frame without border
    Make Frame can be drag
    Create new button for min, max, close

---
+++ Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace TextFilter
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Upload = new System.Windows.Forms.Button();
+            this.Start = new System.Windows.Forms.Button();
+            this.Save = new System.Windows.Forms.Button();
+            this.Area = new System.Windows.Forms.ComboBox();
+            this.Gender = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.close = new System.Windows.Forms.Button();
+            this.min = new System.Windows.Forms.Button();
+            this.max = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Upload
+            // 
+            this.Upload.BackColor = System.Drawing.Color.Red;
+            this.Upload.FlatAppearance.BorderSize = 0;
+            this.Upload.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Upload.Font = new System.Drawing.Font("Impact", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Upload.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Upload.Location = new System.Drawing.Point(45, 80);
+            this.Upload.Name = "Upload";
+            this.Upload.Size = new System.Drawing.Size(135, 24);
+            this.Upload.TabIndex = 0;
+            this.Upload.Text = "Upload";
+            this.Upload.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.Upload.UseVisualStyleBackColor = false;
+            // 
+            // Start
+            // 
+            this.Start.BackColor = System.Drawing.Color.RoyalBlue;
+            this.Start.FlatAppearance.BorderSize = 0;
+            this.Start.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Start.Font = new System.Drawing.Font("Impact", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Start.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Start.Location = new System.Drawing.Point(495, 80);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(135, 24);
+            this.Start.TabIndex = 1;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = false;
+            // 
+            // Save
+            // 
+            this.Save.BackColor = System.Drawing.Color.RoyalBlue;
+            this.Save.FlatAppearance.BorderSize = 0;
+            this.Save.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Save.Font = new System.Drawing.Font("Impact", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Save.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Save.Location = new System.Drawing.Point(645, 80);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(135, 24);
+            this.Save.TabIndex = 2;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = false;
+            // 
+            // Area
+            // 
+            this.Area.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Area.FormattingEnabled = true;
+            this.Area.Location = new System.Drawing.Point(195, 80);
+            this.Area.Name = "Area";
+            this.Area.Size = new System.Drawing.Size(135, 24);
+            this.Area.TabIndex = 3;
+            // 
+            // Gender
+            // 
+            this.Gender.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Gender.FormattingEnabled = true;
+            this.Gender.Location = new System.Drawing.Point(345, 80);
+            this.Gender.Name = "Gender";
+            this.Gender.Size = new System.Drawing.Size(135, 24);
+            this.Gender.TabIndex = 4;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 127);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(960, 414);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label1.Location = new System.Drawing.Point(236, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 23);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Area";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label2.Location = new System.Drawing.Point(376, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 23);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Gender";
+            // 
+            // close
+            // 
+            this.close.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.close.BackColor = System.Drawing.Color.Red;
+            this.close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.close.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.close.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.close.Location = new System.Drawing.Point(895, 5);
+            this.close.Margin = new System.Windows.Forms.Padding(0);
+            this.close.Name = "close";
+            this.close.Size = new System.Drawing.Size(25, 15);
+            this.close.TabIndex = 8;
+            this.close.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.close.UseVisualStyleBackColor = false;
+            this.close.Click += new System.EventHandler(this.close_Click);
+            // 
+            // min
+            // 
+            this.min.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.min.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.min.FlatAppearance.BorderSize = 0;
+            this.min.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.min.Font = new System.Drawing.Font("Agency FB", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.min.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.min.Location = new System.Drawing.Point(925, 5);
+            this.min.Name = "min";
+            this.min.Size = new System.Drawing.Size(25, 15);
+            this.min.TabIndex = 9;
+            this.min.Text = "    ";
+            this.min.UseVisualStyleBackColor = false;
+            this.min.Click += new System.EventHandler(this.min_Click);
+            // 
+            // max
+            // 
+            this.max.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.max.BackColor = System.Drawing.Color.MidnightBlue;
+            this.max.FlatAppearance.BorderSize = 0;
+            this.max.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.max.Location = new System.Drawing.Point(955, 5);
+            this.max.Name = "max";
+            this.max.Size = new System.Drawing.Size(25, 15);
+            this.max.TabIndex = 10;
+            this.max.Text = "    ";
+            this.max.UseVisualStyleBackColor = false;
+            this.max.Click += new System.EventHandler(this.max_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightYellow;
+            this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Controls.Add(this.max);
+            this.Controls.Add(this.min);
+            this.Controls.Add(this.close);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.Gender);
+            this.Controls.Add(this.Area);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.Upload);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Upload;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.ComboBox Area;
+        private System.Windows.Forms.ComboBox Gender;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button close;
+        private System.Windows.Forms.Button min;
+        private System.Windows.Forms.Button max;
+    }
+}
+
